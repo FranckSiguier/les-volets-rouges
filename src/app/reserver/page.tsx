4da526804d2e8e957e9efbba55337c81bbd9f6f2
Franck Siguier <franck.siguier@hungryhungry.com>
2024-12-22 13:37:00 +1100

    Add zenchef iframe to book a table

--- src/app/reserver/page.tsx
+++ src/app/reserver/page.tsx
@@ -1,7 +1,20 @@
+import { SeparatorBanner } from "~/components/separator-banner";
+import { Separator } from "~/components/ui/separator";
+
 export default function BookPage() {
   return (
-    <div className="flex h-[50vh] w-full items-center justify-center">
-      Bientot disponible ...
+    <div className="flex w-full flex-col items-center justify-center gap-6 p-6">
+      <div className="w-full">
+        <Separator />
+        <SeparatorBanner title="Réserver une table" />
+        <Separator />
+      </div>
+      <iframe
+        src="https://bookings.zenchef.com/results?rid=371318&fullscreen=1"
+        width="100%"
+        height="630"
+        className="mx-0 my-auto block h-[630px] max-w-[600px]"
+      />
     </div>
   );
 }
