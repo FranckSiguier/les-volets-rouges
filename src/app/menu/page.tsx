79ceca55e0363313738e0e4f92be38f4197cf069
Franck Siguier <franck.siguier@hungryhungry.com>
2025-01-06 21:27:15 +1100

    Accord mets vins

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -16,7 +16,7 @@ export default async function MenuPage() {
   return (
     <main className="flex w-full flex-col">
       <div className="mb-5 flex flex-col items-center gap-4 md:flex-row md:justify-center md:gap-40 md:py-5 lg:py-10">
-        <div className="flex flex-col gap-2 text-center">
+        <div className="flex flex-col gap-2 text-center text-sm md:text-xl lg:text-2xl">
           <p className="pb-2 font-cormorant text-2xl text-accent md:text-3xl lg:mt-6 lg:pb-6 lg:text-4xl">
             DÉJEUNER
           </p>
@@ -31,21 +31,23 @@ export default async function MenuPage() {
             ENTREE / PLAT / DESSERT : <span className="text-accent">28€</span>
           </p>
         </div>
-        <div className="flex flex-col gap-2 text-center">
+        <div className="flex flex-col gap-2 text-center text-sm">
           <p className="pb-2 font-cormorant text-2xl text-accent lg:mt-6 lg:pb-6 lg:text-4xl">
             DÎNER
           </p>
-          <p className="font-light md:text-2xl">MENU DÉGUSTATION EN 5 TEMPS</p>
+          <p className="font-light md:text-xl lg:text-2xl">
+            MENU DÉGUSTATION EN 5 TEMPS :{" "}
+            <span className="text-accent">58€</span> / PERS
+          </p>
           <p className="font-light opacity-80">
             Une expérience culinaire en 5 étapes, pensée pour être partagée par
             l&apos;ensemble de la table.
           </p>
-          <p className="font-light md:text-2xl">
-            PRIX : <span className="text-accent">58€</span> / PERS
+          <p className="font-light md:text-xl lg:text-2xl">
+            ACCORD METS & VINS : <span className="text-accent">35€</span> / PERS
           </p>
           <p className="font-extralight opacity-80">
             (Ainsi que notre carte habituelle)
-            {/* trggier */}
           </p>
         </div>
       </div>
