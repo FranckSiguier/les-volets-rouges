7160d44c95a32e79e924189364f3f165d7e2ce18
Franck Siguier <franck.siguier@hungryhungry.com>
2025-01-06 21:11:59 +1100

    Changer la formule

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -18,24 +18,33 @@ export default async function MenuPage() {
       <div className="mb-5 flex flex-col items-center gap-4 md:flex-row md:justify-center md:gap-40 md:py-5 lg:py-10">
         <div className="flex flex-col gap-2 text-center">
           <p className="pb-2 font-cormorant text-2xl text-accent md:text-3xl lg:mt-6 lg:pb-6 lg:text-4xl">
-            Le Midi
+            DÉJEUNER
           </p>
           <p className="font-light md:text-2xl">
-            Plat du Jour <span className="text-accent">18€</span>
+            PLAT DU JOUR : <span className="text-accent">18€</span>
           </p>
           <p className="font-light md:text-2xl">
-            Plat du Jour + Dessert <span className="text-accent">24€</span>
+            ENTREE / PLAT <span className="opacity-80">OU</span> PLAT / DESSERT
+            : <span className="text-accent">24€</span>
+          </p>
+          <p className="font-light md:text-2xl">
+            ENTREE / PLAT / DESSERT : <span className="text-accent">28€</span>
           </p>
         </div>
         <div className="flex flex-col gap-2 text-center">
           <p className="pb-2 font-cormorant text-2xl text-accent lg:mt-6 lg:pb-6 lg:text-4xl">
-            Le Soir
+            DÎNER
+          </p>
+          <p className="font-light md:text-2xl">MENU DÉGUSTATION EN 5 TEMPS</p>
+          <p className="font-light opacity-80">
+            Une expérience culinaire en 5 étapes, pensée pour être partagée par
+            l&apos;ensemble de la table.
           </p>
           <p className="font-light md:text-2xl">
-            Menu dégustation en 5 temps <span className="text-accent">58€</span>
+            PRIX : <span className="text-accent">58€</span> / PERS
           </p>
-          <p className="font-extralight md:text-2xl">
-            (Ainsi que la carte ci-dessous)
+          <p className="font-extralight opacity-80">
+            (Ainsi que notre carte habituelle)
           </p>
         </div>
       </div>
